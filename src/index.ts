0e1c50e2071cf7485f484dbfbde932c732f947ef
Vivek <vivekpradhan736@gmail.com>
2024-08-14 00:40:20 +0530

    new commit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Masonry } from './components/Masonry';
-export { default as ColoumMasonry } from './components/ColoumMasonry';
+// export { default as ColoumMasonry } from './components/ColoumMasonry';
+export * from "./components/ColoumMasonry";
